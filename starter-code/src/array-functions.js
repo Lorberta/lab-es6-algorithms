f5607c7f50d9fc4bd62f597d5fdc1c0b81374e8a
Johanna <johanna.skuhra@gmail.com>
2018-10-31 10:41:40 +0100

    half done

--- starter-code/src/array-functions.js
+++ starter-code/src/array-functions.js
@@ -1,4 +1,4 @@
-var ArrayFunctions = function (){}
+var ArrayFunctions = function () { }
 
 // 1) Define a function that takes an array with numbers and prints all the elements of the array, separated by " --- "
 // ArrayFunctions.printSpecial([12, 33, 144, 122])
@@ -12,8 +12,8 @@ ArrayFunctions.prototype.printSpecial = function (array) {
 // ArrayFunctions.doubleMyArray([10, 20, 35, 12])
 // [20, 40, 70, 24]
 
-ArrayFunctions.prototype.doubleArray = function(array){
-  return array.map(function(number){
+ArrayFunctions.prototype.doubleArray = function (array) {
+  return array.map(function (number) {
     return number * 2;
   });
 };
@@ -24,8 +24,8 @@ ArrayFunctions.prototype.doubleArray = function(array){
 // explanation: (1 x 10^0) + (2 x 10^1) + (3 x 10^2) + (4 x 10^3) + (5 x 10^4)
 // explanation: (1)        + (20)       + (300)      + (4000)     + (50000)
 
-ArrayFunctions.prototype.superPower = function(array){
-  return array.reduce(function(sum, number, index){
+ArrayFunctions.prototype.superPower = function (array) {
+  return array.reduce(function (sum, number, index) {
     return sum + (number * (Math.pow(10, index)));
   });
 }
